b2ad5049cb8894f95ff81a264c868fd40562fe60
charly <carlosgarita3110@gmail.com>
2022-09-16 16:49:10 -0600

    Definition of bus class attributes

---
+++ TicaBus/Bus.h
@@ -0,0 +1,13 @@
+#include "Asiento.h"
+
+class Bus {
+private:
+    string placa;
+    string modelo;
+    string marca;
+    double costoDelAsiento;
+    Asiento** vecA;
+    int cantidad;
+    int tamano;
+public:
+};
