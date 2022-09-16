559f00ef442924dbc7b3cf15846e014d59ed1585
charly <carlosgarita3110@gmail.com>
2022-09-16 17:27:12 -0600

    definition of CompaniaTicaBus class methods

--- TicaBus/CompaniaTicaBus.h
+++ TicaBus/CompaniaTicaBus.h
@@ -7,4 +7,17 @@ private:
     Bus** vecB;
     int cantidad;
     int tamano;
+public:
+    CompaniaTicaBus(string nombreCom = "", string telefono = "", int tamano = 10);
+    virtual ~CompaniaTicaBus();
+    string getNombreCom();
+    string getTelefono();
+    Bus** getVecB();
+    int getCantidad();
+    int getTamano();
+    void setNombreCom(string nombreCom);
+    void setTelefono(string telefono);
+    void agregarBus(Bus* b);
+    void eliminarBus(string placa);
+    string toString();
 };
